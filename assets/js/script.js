606b5133895684c47241a2f4e43ad9480ffd9169
opoku1968 <>
2023-09-28 11:04:06 +0000

    add to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -182,4 +182,51 @@ const winner = (player, computer) => {
 };
 
 
+// Function to run when game is over
+const gameOver = (playerOptions, movesLeft) => {
+
+    const chooseMove = document.querySelector('.move');
+    const result = document.querySelector('.result');
+    const reloadBtn = document.querySelector('.reload');
+
+    playerOptions.forEach(option => {
+        option.style.display = 'none';
+    });
+
+
+    chooseMove.innerText = 'Game Over!!';
+    movesLeft.style.display = 'none';
+
+    if (playerScore > computerScore) {
+        result.style.fontSize = '2rem';
+        result.innerText = 'You Won The Game';
+        result.style.color = '#308D46';
+    }
+    else if (playerScore < computerScore) {
+        result.style.fontSize = '2rem';
+        result.innerText = 'You Lost The Game';
+        result.style.color = 'red';
+    }
+    else {
+        result.style.fontSize = '2rem';
+        result.innerText = 'Tie';
+        result.style.color = 'grey';
+    }
+    reloadBtn.innerText = 'Restart';
+    reloadBtn.style.display = 'flex';
+    reloadBtn.addEventListener('click', () => {
+        window.location.reload();
+    });
+};
+
+
+// Calling playGame function inside game
+playGame();
+
+
+
+// Calling the game function
+game();
+
+
 
